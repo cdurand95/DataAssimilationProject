7621bfd718362d5a2bf55f5508dda71abc7c39d5
ojacquot <94471434+ojacquot@users.noreply.github.com>
2021-12-03 17:11:08 +0100

    learning rate

--- gridsearch_sparsity1.py
+++ gridsearch_sparsity1.py
@@ -25,7 +25,9 @@ dW_list = [1, 2, 4, 8]
 dimCNN = 10
 
 sparsity = 1
-max_epochs = 50
+max_epochs = 100
+
+lr = 5e-3
 
 ### Data generation
 
@@ -42,23 +44,23 @@ print('Test       : ' + str(data[2]['Truth'].shape))
 
 ### Define model for a set of hyperparameters
 
-def define_model(data, n_layers, dW, dimCNN):
-    model = utils.CNN(data, n_layers=n_layers, dW=dW, dimCNN=dimCNN)
+def define_model(data, n_layers, dW, dimCNN, lr):
+    model = utils.CNN(data, n_layers=n_layers, dW=dW, dimCNN=dimCNN, lr=lr)
     return model
 
 ### Train model
 
 def train_model(model, max_epochs):
 
-    early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=0.00, patience=3, verbose=False, mode='max')
+    early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=0.00, patience=5, verbose=False, mode='min')
 
     if device.type == 'cuda':
         trainer = pl.Trainer(max_epochs=max_epochs, callbacks=[early_stop_callback], gpus=1, auto_select_gpus=True)
     else :
         trainer = pl.Trainer(max_epochs=max_epochs, callbacks=[early_stop_callback])
 
-
     trainer.fit(model)
+
     return None
 
 ### Save training data
@@ -68,7 +70,8 @@ def train_model(model, max_epochs):
 def gridsearch():
 
     path = 'L63gridsearch/sparsity{}'.format(sparsity)
-    os.makedirs(path)
+    if not(os.path.isdir(path)):
+        os.makedirs(path)
 
     hp_cart_product = itertools.product(n_layers_list, dW_list)
 
@@ -78,14 +81,14 @@ def gridsearch():
 
         print('Building CNN model with {} layers and a convolution kernel of half-width {}'.format(n_layers, dW))
 
-        model = define_model(data, n_layers, dW, dimCNN)
+        model = define_model(data, n_layers, dW, dimCNN, lr)
 
         print(model)
         print('Number of trainable parameters = %d'%(sum(p.numel() for p in model.parameters() if p.requires_grad)))
 
         train_model(model, max_epochs)
 
-        torch.save(model, path + '/model_n{}_dW{}_epoch{}.pth'.format(n_layers,dW, max_epochs))
+        torch.save(model, path + '/model_n{}_dW{}_epoch{}.pth'.format(n_layers, dW, max_epochs))
         # model = torch.load('model.pth')
 
         hp_perf.append((model.tot_loss[-1], n_layers, dW))
@@ -95,7 +98,7 @@ def gridsearch():
     with open(path + '/perf_{}epochs.txt'.format(max_epochs), 'w') as file:
         for loss, n_layers, dW in hp_perf[::-1] :
             print('Loss : {:.2f}, n_layers : {}, dw : {}'.format(loss, n_layers, dW))
-            file.write('Loss : {:.2f}, n_layers : {}, dw : {}'.format(loss, n_layers, dW))
+            file.write('Loss : {:.2f}, n_layers : {}, dw : {}\n'.format(loss, n_layers, dW))
 
     return None
 
